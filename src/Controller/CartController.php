1b06dac7b400eebdbf3aead33e0e646587931d42
Jean Sérien <clavinova1@free.fr>
2025-09-10 14:24:08 +0200

    Début transformation CartController en requêtes Ajax

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -4,7 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Offres;
 use App\Repository\OffresRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
@@ -32,12 +35,44 @@ class CartController extends AbstractController
 
             $total += $offre->getPrix() * $quantite;
         }
-        return $this->render('cart/index.html.twig', compact('data','total'));
+        return $this->render('cart/index.html.twig', compact('data', 'total'));
     }
 
     //Gestion des ajouts dans le panier
     #[Route('/add/{id}', 'add')]
-    public function add(Offres $offre, SessionInterface $session)
+    public function add(Offres $offre, SessionInterface $session, Request $request): JsonResponse
+    {
+        //Vérification qu'il s'agît bien d'une requête Ajax
+        if ($request->isXmlHttpRequest()) {
+
+            //Récupération de l'id de l'offre
+            $id = $offre->getId();
+
+            //Récupération du panier si il existe déjà
+            $panier = $session->get('panier', []);
+
+            //Ajout de l'offre dans le panier si non existante 
+            //ou bien l'on incrémente sa quantité
+
+            if (empty($panier[$id])) {
+                $panier[$id] = 1;
+            } else {
+                $panier[$id]++;
+            }
+
+            $session->set('panier', $panier);
+
+            //Affichage du partial du panier en Ajax
+
+            return new JsonResponse(['content' => $this->renderView('_partials/_cart-items.html.twig')]);
+        } else {
+            return new Jsonresponse('La reqûete doit s\'effectuer en Ajax');
+        }
+    }
+
+    //Gestion des suppressions d'offre dans le panier
+    #[Route('/remove/{id}', 'remove')]
+    public function remove(Offres $offre, OffresRepository $offresRepo, EntityManagerInterface $em, SessionInterface $session, $id)
     {
         //Récupération de l'id de l'offre
         $id = $offre->getId();
@@ -45,19 +80,50 @@ class CartController extends AbstractController
         //Récupération du panier si il existe déjà
         $panier = $session->get('panier', []);
 
-        //Ajout de l'offre dans le panier si non existante 
-        //ou bien l'on incrémente sa quantité
+        //Suppression de l'offre dans le panier si un seul exemplaire 
+        //ou bien l'on décrémente sa quantité
 
-        if (empty($panier[$id])) {
-            $panier[$id] = 1;
-        } else {
-            $panier[$id]++;
+        if (!empty($panier[$id])) {
+            if ($panier[$id] > 1) {
+                $panier[$id]--;
+            } else {
+                unset($panier[$id]);
+            }
+        }
+        $session->set('panier', $panier);
+
+        //Redirection vers la page du panier
+        return $this->redirectToRoute('cart_index');
+    }
+    //Gestion des suppressions d'offre dans le panier
+    #[Route('/delete/{id}', 'delete')]
+    public function delete(Offres $offre, SessionInterface $session, $id)
+    {
+        //Récupération de l'id de l'offre
+        $id = $offre->getId();
+
+        //Récupération du panier si il existe déjà
+        $panier = $session->get('panier', []);
+
+        //Suppression de l'offre dans le panier si un seul exemplaire 
+        //ou bien l'on décrémente sa quantité
+
+        if (!empty($panier[$id])) {
+            unset($panier[$id]);
         }
 
         $session->set('panier', $panier);
 
         //Redirection vers la page du panier
+        return $this->redirectToRoute('cart_index');
+    }
 
+    //Gestion de vidage panier
+    #[Route('/empty-cart', 'empty_cart')]
+    public function emptyCart(SessionInterface $session): Response
+    {
+        $session->remove('panier');
+        //Redirection vers la page du panier
         return $this->redirectToRoute('cart_index');
     }
 }
